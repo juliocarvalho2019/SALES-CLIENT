844f5c8488a70b8f913067bd4bf95d4985d430d5
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-04-26 17:38:03 -0300

    Feat: Fazendo parte de requisição a lista de produtos com coroutines.

--- app/src/main/java/br/com/carvalho/salesclient/MainActivity.kt
+++ app/src/main/java/br/com/carvalho/salesclient/MainActivity.kt
@@ -2,10 +2,15 @@ package br.com.carvalho.salesclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import br.com.carvalho.salesclient.product.ProductListViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewModel = ViewModelProvider(this).get(ProductListViewModel::class.java)
+
     }
 }
--- app/src/main/java/br/com/carvalho/salesclient/product/ProductListViewModel.kt
+++ app/src/main/java/br/com/carvalho/salesclient/product/ProductListViewModel.kt
@@ -7,7 +7,33 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+
 private const val TAG = "ProductListViewModel"
+private var viewModelJob = Job()
+private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
 
 class ProductListViewModel : ViewModel() {
+
+    init {
+        getProducts()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+    private fun getProducts() {
+        Log.i(TAG, "Preparing to request products list")
+        coroutineScope.launch {
+            val getProductsDeferred = SalesApi.retrofitService.getProducts()
+            Log.i(TAG, "Loading products")
+            val productsList = getProductsDeferred.await()
+            Log.i(TAG, "Number of products ${productsList.size}")
+        }
+        Log.i(TAG, "Products list requested")
+    }
+
+
 }
