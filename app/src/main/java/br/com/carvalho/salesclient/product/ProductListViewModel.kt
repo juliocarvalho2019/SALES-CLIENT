af9beb5e655556b1f1967966f0004f46300c9ae4
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-04-26 17:16:00 -0300

    Feat: Criado pacote product e classe pra viewmodel de produtos.

---
+++ app/src/main/java/br/com/carvalho/salesclient/product/ProductListViewModel.kt
@@ -0,0 +1,13 @@
+package br.com.carvalho.salesclient.product
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import br.com.carvalho.salesclient.network.SalesApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+private const val TAG = "ProductListViewModel"
+
+class ProductListViewModel : ViewModel() {
+}
